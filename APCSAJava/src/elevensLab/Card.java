0f779995399b139bd2a0cd5197ebeb676477df4b
Cody <Cody@CodySurface>
2020-02-27 16:35:10 -0800

    Finished All of Card and Deck activity

--- APCSAJava/src/elevensLab/Card.java
+++ APCSAJava/src/elevensLab/Card.java
@@ -9,20 +9,77 @@ public class Card
 			"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
 
 	//instance variables
-		//String suit
-		//int face
+		private String rank, suit;
+		private int pointValue;
+		private int face = -1;
 
   	//constructors
+	public Card(String suit, int face) {
+		this.suit = suit;
+		this.face = face;
+	}
+
+	public Card(String rank, String suit, int pointValue){
+		this.rank = rank;
+		this.suit = suit;
+		this.pointValue = pointValue;
+	}
 
 
 	// modifiers
-		//set methods
+	public void setRank(String rank){
+		this.rank = rank;
+	}
+	public void setSuit(String suit){
+		this.suit = suit;
+	}
+	public void setPointValue(int pointValue){
+		this.pointValue = pointValue;
+	}
+	public void setFace(int face) {
+		this.face = face;
+	}
+	
 
 
   	//accessors
-		//get methods
-
+	public String getRank(){
+		return rank;
+	}
+	public String getSuit() {
+		return suit;
+	}
+	public int getPointValue() {
+		return pointValue;
+	}
+	public int getFace() {
+		return face;
+	}
+	
+	
+	//methods
+	public boolean isSame(Card a, Card b) {
+		if (a.rank == b.rank) {
+			if (a.suit == b.suit) {
+				if (a.pointValue == b.pointValue) {
+					return true;
+				}
+			return false;	
+			}
+		return false;	
+		}
+	return false;
+	}
 
   	//toString
+	public String toString(){
+		if (rank != null) {
+			return (rank + " of " + suit + " (point value = " + pointValue + ")");
+		}
+		if (face != -1) {
+			return (FACES[face] + " of " + suit);
+		}
+		return "";
+	}
 
  }
--- APCSAJava/src/elevensLab/Deck.java
+++ APCSAJava/src/elevensLab/Deck.java
@@ -1,3 +1,4 @@
+package elevensLab;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
@@ -8,24 +9,45 @@ import java.util.Arrays;
 import java.util.Collections;
 
 //make a Deck class
-
+public class Deck{
 	public static final int NUMCARDS = 52;
 	public static String[] SUITS = "CLUBS HEARTS DIAMONDS SPADES".split(" ");
 	
 	private List<Card> cards;
 	private int top;
-
-   //make a Deck constructor
+	//Deck Constructors
+	public Deck(){
    	//refer cards to new ArrayList
+		cards = new ArrayList<Card>();
    	//set top to the top of the deck 51
-   	
+		top = 51;
    	//loop through all suits
-   		//loop through all faces 1 to 13
+		for (int i = 0; i < SUITS.length; i ++) {
+			//loop through all faces 1 to 13
+			for (int j = 1; j < 14; j ++) {
+				cards.add(new Card(SUITS[i], j));
+			}
+		}
    			//add a new TwentyOneCard to the deck
 
-   
+	}
+	
+	
+	
    //make a dealCard() method that returns the top card
-   
+   public Card dealCard() {
+	   top -= 1;
+	   return cards.get(top + 1);
+   }
    //write a shuffle() method
-   	//use Colletions.shuffle
-   	//reset the top card 
+   public void shuffle() {
+	   //use Colletions.shuffle
+	   Collections.shuffle(cards);
+	   //reset the top card 
+	   top = 51;
+   }
+   
+
+
+	
+}
